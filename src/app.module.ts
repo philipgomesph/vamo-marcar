8ceb0ebd2c2e95457ecd27f036b048d94d3914a9
philipgomesph <philip.gomesph@gmail.com>
2023-06-12 23:44:09 -0300

    separando branch prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ClientModule } from './modules/client/client.module';
 import { RepositoryService } from './database/repository/repository.service';
 import { RepositoryModule } from './database/repository/repository.module';
+import { PrismaModule } from './prisma/prisma/prisma.module';
 
 @Module({
-  imports: [ClientModule, RepositoryModule],
+  imports: [ClientModule, RepositoryModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, RepositoryService],
 })
--- src/modules/client/client.service.ts
+++ src/modules/client/client.service.ts
@@ -2,16 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { RepositoryService } from 'src/database/repository/repository.service';
 import { IClient } from './client.dto';
 import { serverTimestamp } from 'firebase/firestore';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
 
 @Injectable()
 export class ClientService {
-  constructor(private repositoryService: RepositoryService) {}
+  constructor(
+    private repositoryService: RepositoryService,
+    private prisma: PrismaService,
+  ) {}
   async createClient(data: IClient) {
     data.createAt = new Date();
+    data.deleteAt = new Date();
     console.log(data);
-    const docRef = this.repositoryService.create(data, 'clients');
+    const prismaCreate = this.prisma.modelClient.create({
+      data: {
+        email: data.email,
+        name: data.name,
+        id: '123',
+      },
+    });
+    //const docRef = this.repositoryService.create(data, 'clients');
 
-    return docRef;
+    console.log(prismaCreate);
+    return prismaCreate;
   }
 
   async showClient(id: string) {
